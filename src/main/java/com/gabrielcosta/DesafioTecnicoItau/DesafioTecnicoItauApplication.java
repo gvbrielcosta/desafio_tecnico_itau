ae8ac6201c61ae25017da038b872a0e997e4197a
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-26 19:37:09 -0300

    first commit

---
+++ src/main/java/com/gabrielcosta/DesafioTecnicoItau/DesafioTecnicoItauApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielcosta.DesafioTecnicoItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielcosta/DesafioTecnicoItau/DesafioTecnicoItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcosta.DesafioTecnicoItau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
